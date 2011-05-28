35d6cfe9d460edc952af7c421598d1eb160b67ed
Kerry Buckley <kerryjbuckley@gmail.com>
2011-05-28 09:15:07 +0100

    Refactor case statement using join

--- lib/numbers_as_words.rb
+++ lib/numbers_as_words.rb
@@ -1,19 +1,18 @@
 module NumbersAsWords
-  UNITS = %w{zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
-  TENS = %w{_ _ twenty thirty forty fifty sixty seventy eighty ninety}
+  UNITS = [nil] + %w{one two three four five six seven eight nine ten} +
+    %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
+  TENS = [nil, nil] + %w{twenty thirty forty fifty sixty seventy eighty ninety}
 
   def as_words
     case self
-    when 0..19
+    when 0
+      "zero"
+    when 1..19
       UNITS[self]
-    when *((20..90).step 10)
-      TENS[self/10]
     when 20..99
-      "#{TENS[self/10]}-#{UNITS[self % 10]}"
-    when *((100..900).step 100)
-      "#{UNITS[self/100]} hundred"
+      [TENS[self/10], UNITS[self % 10]].compact.join "-"
     else
-      "#{UNITS[self/100]} hundred and #{(self % 100).as_words}"
+      (["#{UNITS[self/100]} hundred", (self % 100).as_words] - [nil, "zero"]).join " and "
     end
   end
 end
