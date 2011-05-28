416339dcca12165eacae517d77743cc78a94ebb8
Kerry Buckley <kerryjbuckley@gmail.com>
2011-05-28 09:39:39 +0100

    Refactor. No longer handle zero.

--- lib/numbers_as_words.rb
+++ lib/numbers_as_words.rb
@@ -1,18 +1,30 @@
 module NumbersAsWords
-  UNITS = [nil] + %w{one two three four five six seven eight nine ten} +
+  UNITS_AND_TEENS = [nil] + %w{one two three four five six seven eight nine ten} +
     %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
   TENS = [nil, nil] + %w{twenty thirty forty fifty sixty seventy eighty ninety}
 
   def as_words
     case self
     when 0
-      "zero"
-    when 1..19
-      UNITS[self]
-    when 20..99
-      [TENS[self/10], UNITS[self % 10]].compact.join "-"
+      nil
+    when 1..99
+      [tens, units_and_teens].compact.join "-"
     else
-      (["#{UNITS[self/100]} hundred", (self % 100).as_words] - [nil, "zero"]).join " and "
+      [hundreds, (self % 100).as_words].compact.join " and "
     end
+  end
+
+  private
+
+  def units_and_teens
+    UNITS_AND_TEENS[self] || UNITS_AND_TEENS[self % 10]
+  end
+
+  def tens
+    TENS[self/10]
+  end
+
+  def hundreds
+    "#{UNITS_AND_TEENS[self/100]} hundred"
   end
 end
--- spec/numbers_as_words_spec.rb
+++ spec/numbers_as_words_spec.rb
@@ -6,7 +6,7 @@ Integer.send :include, NumbersAsWords
 
 describe "Numbers as words" do
   context "single digits" do
-    {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
+    {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
       6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}.each do |number, word|
 
       it "converts #{number} to '#{word}'" do
