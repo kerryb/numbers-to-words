e98c7130b9a477db655d7df017ea652a93bbf0f6
Kerry Buckley <kerryjbuckley@gmail.com>
2012-10-19 19:32:42 +0100

    Failing spec for 2 -> 2

--- spec/converts_numbers_to_words_spec.rb
+++ spec/converts_numbers_to_words_spec.rb
@@ -4,4 +4,8 @@ describe ConvertsNumberToWords do
   it "converts 1 to 'one'" do
     expect(ConvertsNumberToWords.for(1).as_words).to eq "one"
   end
+
+  it "converts 2 to 'two'" do
+    expect(ConvertsNumberToWords.for(2).as_words).to eq "two"
+  end
 end
