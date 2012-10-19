0d3bb71766b25b471979414699b203c139552337
Kerry Buckley <kerryjbuckley@gmail.com>
2012-10-19 20:35:05 +0100

    Failing specs for 3 to 19

--- spec/converts_numbers_to_words_spec.rb
+++ spec/converts_numbers_to_words_spec.rb
@@ -12,7 +12,24 @@ describe ConvertsNumberToWords do
   end
 
   { 1 => "one",
-    2 => "two"
+    2 => "two",
+    3 => "three",
+    4 => "four",
+    5 => "five",
+    6 => "six",
+    7 => "seven",
+    8 => "eight",
+    9 => "nine",
+    10 => "ten",
+    11 => "eleven",
+    12 => "twelve",
+    13 => "thirteen",
+    14 => "fourteen",
+    15 => "fifteen",
+    16 => "sixteen",
+    17 => "seventeen",
+    18 => "eighteen",
+    19 => "nineteen"
   }.each do |number, words|
     it { should convert(number, to: words) }
   end
