ac43e8b0833bc5686eea519dbdf48e9c20c13803
Kerry Buckley <kerryjbuckley@gmail.com>
2012-10-19 20:07:34 +0100

    Add slightly gratuitous custom matcher

--- spec/converts_numbers_to_words_spec.rb
+++ spec/converts_numbers_to_words_spec.rb
@@ -1,11 +1,15 @@
 require "converts_number_to_words"
 
 describe ConvertsNumberToWords do
+  subject { ConvertsNumberToWords }
+
+  RSpec::Matchers.define :convert do |number, args|
+    match {|convertor| convertor.for(number).as_words == args.fetch(:to) }
+  end
+
   { 1 => "one",
     2 => "two"
   }.each do |number, words|
-    it "converts #{number} to '#{words}'" do
-      expect(ConvertsNumberToWords.for(number).as_words).to eq words
-    end
+    it { should convert(number, to: words) }
   end
 end
